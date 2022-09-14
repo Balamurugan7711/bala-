93aecc7b60ff8dc0ebaa32f822d75ad1f6df5134
Balamurugan7711 <112743045+Balamurugan7711@users.noreply.github.com>
2022-09-14 13:58:53 +0530

    Create D3 program7.py

---
+++ D3 program7.py
@@ -0,0 +1,16 @@
+from itertools import permutations
+
+p= list((input("how many number u want to add:")))
+
+comb = permutations(p, len(p))
+
+for k in comb:
+
+    print(k)
+
+Footer
+
+© 2022 GitHub, Inc.
+
+Footer navigation
+
